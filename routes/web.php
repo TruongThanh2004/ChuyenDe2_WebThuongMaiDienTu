2d8d33569bc14bcfff7076ed540ff322b387c3b7
TruongThanh <thanhdoken113@gmail.com>
2024-10-19 08:24:14 +0700

    b

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
