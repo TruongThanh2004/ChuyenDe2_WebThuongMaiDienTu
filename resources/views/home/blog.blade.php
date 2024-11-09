d7c36d10da0d4ef10bf3d220cbe1ca995aa1015f
vansieu2502 <nguyenvansieu2502@gmail.com>
2024-11-09 09:56:36 +0700

    fix css

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -13,7 +13,9 @@
         
     }
 </style>
-
+<head>
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
 <section id="page-header" class="blog-header">
     <h2>#readmore</h2>
     <p>Read all case studies about our products!</p>
