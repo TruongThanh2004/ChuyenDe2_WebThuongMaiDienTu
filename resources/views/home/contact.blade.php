a45fd747f71de4f80d3eda6d7f0d95d9da4be7f6
@Khanh08052004 <nguyenduckhanh0805@gmail.com>
2024-11-21 08:15:39 +0700

    map

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -16,15 +16,15 @@
             <div>
                 <li>
                     <i class="fas fa-map"></i>
-                    <p>Lahore, Pakistan 54840</p>
+                    <p>21/2A Phan Huy Ích, Phường 12, Gò Vấp, Thành phố Hồ Chí Minh, Việt Nam</p>
                 </li>
                 <li>
                     <i class="fas fa-envelope"></i>
-                    <p>connectwithmawais@gmail.com</p>
+                    <p>0234 678 678</p>
                 </li>
                 <li>
                     <i class="fas fa-phone-alt"></i>
-                    <p>+92-321-4655990
+                    <p>didongustors@gmail.com
                 </li>
                 <li>
                     <i class="fas fa-clock"></i>
@@ -34,12 +34,11 @@
         </div>
         <div class="map">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d217759.48983392344!2d74.1943055256652!3d31.483156882376644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39190483e58107d9%3A0xc23abe6ccc7e2462!2sLahore%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1676498542792!5m2!1sen!2s"
+                src="https://www.google.com/maps?q=Thương+mại+điện+tử,+21/2A+Phan+Huy+Ích,+Phường+12,+Gò+Vấp,+Thành+phố+Hồ+Chí+Minh,+Việt+Nam&output=embed"
                 width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
-
     <section id="form-details">
         <form action="">
             <span>LEAVE A MESSAGE</span>
